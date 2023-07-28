92c106f0f754ab39fbd30fec7e26b56f970b1731
joaosantos <joaovitor.d370@gmail.com>
2023-07-28 11:25:44 -0300

    tela de perfil

---
+++ esportes/src/Controller/PerfilController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PerfilController extends AbstractController
+{
+    #[Route('/perfil', name: 'app_perfil')]
+    public function index(): Response
+    {
+        return $this->render('perfil/index.html.twig', [
+            'controller_name' => 'PerfilController',
+        ]);
+    }
+}
